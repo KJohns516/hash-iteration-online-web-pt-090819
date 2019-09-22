a9a3f6088a71e3aebf0712df4efa8eb649ee4a9e
Kristan Johns <kristandjohns@gmail.com>
2019-09-22 21:27:33 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -2,7 +2,8 @@ require_relative "../lib/birthday.rb"
 
 describe "#happy_birthday" do
   it "prints out a birthday message to each kid in the birthday_kids hash" do
-    birthday_kids = {
+    birthday_kids = {#happy_birthday}
+  def birthday_kids
       "Timmy" => 9,
       "Sarah" => 6,
       "Amanda" => 27
